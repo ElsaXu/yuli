98459b37b0e96c38a2ba180a0e51d74c02b6bcc8
zhuren <zhuren>
2018-11-11 02:44:18 +0800

    Use rem instead of px to implement responsive layout.

--- js/home.js
+++ js/home.js
@@ -3,9 +3,11 @@
  * Start 
  */ 
 var win = $(window);
+var body = $('html');
 var container1 = $('.container-1');
 var rect1 = $('.rect-1');
 var bg1 = $('.bg-1');
+var title1 = $('.title-1');
 /* 
  * Visual dom elements definition
  * End
@@ -19,12 +21,13 @@ var widthRate = 1; // The real window width / 1920
 var defaultWidth = 1920;
 var scrollTop = 0;
 var config = {
+	body_fontSize: 16,
 	bg_1_height: 2133,
-	bg_1_hideAt: 1700,
+	bg_1_hideAt: 1100,
 	rect_1_showAt: 200,
 	rect_1_hideAt: 1200,
 	rect_1_widthRate: 0.65,
-	rect_1_heightRate: 2.2,
+	title_1_hideAt: 0,
 }
 /* 
  * Parameters definition
@@ -34,9 +37,13 @@ var config = {
 /* 
  * Init
  * Start 
- */ 
-windowResized();
-windowScrolled();
+ */
+
+setTimeout(function() {
+	win.scrollTop(0); 
+	windowResized();
+	windowScrolled();
+});
 /* 
  * Init
  * End 
@@ -65,8 +72,9 @@ function windowResized() {
 	var _width = $(window).width();
 	scrollTop = win.scrollTop();
 	widthRate = _width / defaultWidth;
-	container1.css('height', config.bg_1_height * widthRate);
-	rect1.css('height', _width * config.rect_1_widthRate / config.rect_1_heightRate);
+	body.css('font-size', (config.body_fontSize * widthRate) + 'px');
+	// container1.css('height', config.bg_1_height * widthRate);
+	// rect1.css('height', _width * config.rect_1_widthRate / config.rect_1_heightRate);
 }
 function windowScrolled() {
 	scrollTop = win.scrollTop();
@@ -90,12 +98,25 @@ function checkRect1() {
 	var _hideAt = config.rect_1_hideAt * widthRate;
 	var _rate;
 	if (_scrollY > _showAt) {
-		_rate = Math.max(0, (_hideAt - _scrollY) / (_hideAt - _showAt));  
-		rect1.css('width', (config.rect_1_widthRate * _rate * 100) + '%');
+		_rate = Math.max(0, (_hideAt - _scrollY) / (_hideAt - _showAt)) - 1;  
 	} else {
-		rect1.css('width', (config.rect_1_widthRate * 100) + '%');
+		_rate = 0;
+	}
+	var _dx = _rate * rect1.width();
+	rect1.css('right', _dx);
+	checkTitle1();
+	function checkTitle1() {
+		var _width = $(window).width();
+		var _hideAt2 = config.title_1_hideAt * widthRate;
+		if (_scrollY > _hideAt2) {
+			title1.css('top', _hideAt2 - _scrollY);
+		} else {
+			title1.css('top', 0);
+		}
+		title1.css('left', _dx);
 	}
 }
+
 /* 
  * Function definition
  * End 
