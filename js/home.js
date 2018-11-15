ea1064486b51555b61ca610a20a27fa9d1952bca
ext.elsa.xu <ext.elsa.xu@ef.com>
2018-11-15 22:28:24 +0800

    complete animation of scenario 4.

--- js/home.js
+++ js/home.js
@@ -227,6 +227,7 @@ function checkBg2() {
 			if (title2.hasClass('bg2-title-1-short-ani')) {
 				title2.removeClass('bg2-title-1-short-ani').addClass('bg2-title-1-long-ani');
 			}
+			displayBg2BigSquare();
 			displayBg2WidgetAni();
 			title2.css('top', title2.data('topToContainer2'));
 		}
@@ -289,6 +290,8 @@ function checkBg4() {
 	var _top = container4.data('toTop') - scrollTop;
 	container4.css('top', _top);
 	container4Inner.css('top', _top + container4Inner.data('toTop'));
+	// displayBg4WidgetAni();
+	// displayBg2WidgetAni();
 	// if (_top <= windowHeight - _bg3StartAt) {
 	// 	// show bg3
 	// 	bg3.css('transform', 'scale(1, 1)');
@@ -309,40 +312,61 @@ setTimeout(function() {
 	bg3title2.addClass("bg3-title-ani");
 },5000);
 
-function displayBg2WidgetAni() {
-	// 大正方形 环绕
+function displayBg2BigSquare() {
 	var squareTop = $('.bg2-title-1 .line-top');
 	var squareBottom = $('.bg2-title-1 .line-bottom-70');
 	var squareLeft = $('.bg2-title-1 .line-left');
 	var squareRight = $('.bg2-title-1 .line-right');
 
+	squareTop.stop(true, true);
+	squareBottom.stop(true, true);
+	squareLeft.stop(true, true);
+	squareRight.stop(true, true);
+
+	squareTop.addClass("line-top-animate");
+	squareBottom.addClass("line-bottom-animate");
+	squareLeft.addClass("line-left-animate");
+	squareRight.addClass("line-right-animate");
+}
+
+function displayBg2WidgetAni() {
 	// xiao正方形 环绕
 	var square1Top = $('.square-container-1 .line-top-reverse');
 	var square1Bottom = $('.square-container-1 .line-bottom-100');
 	var square1Left = $('.square-container-1 .line-left-reverse');
 	var square1Right = $('.square-container-1 .line-right-reverse');
-
 	// 三角形 放大缩小
 	var trangle = $('.square-container-1 > .trangle-container');
-	//  small text
 	var smallText = $('.bg2-title-2 .bg2-text-mask');
 
-	// 大正方形 环绕
-	console.log(square1Top.length);
-	squareTop.addClass("line-top-animate");
-	squareBottom.addClass("line-bottom-animate");
-	squareLeft.addClass("line-left-animate");
-	squareRight.addClass("line-right-animate");
-
+	square1Top.stop(true,true);
+	square1Bottom.stop(true,true);
+	square1Left.stop(true,true);
+	square1Right.stop(true, true);
+	trangle.stop(true,true);
+	smallText.stop(true,true);
 	// 三角形 放大缩小
 	trangle.addClass("trangle-scale-ani");
 	smallText.addClass("bg2-text-ani");
-	
 	// xiao正方形 环绕
 	square1Top.addClass("line-top-animate-1");
 	square1Bottom.addClass("line-bottom-animate-1");
 	square1Left.addClass("line-left-animate-1");
 	square1Right.addClass("line-right-animate-1");
+}
+
+function displayBg4WidgetAni() {
+	var bg4Image1 = $(".bg4-image-container .bg4-image-1");
+	var bg4Image2 = $(".bg4-image-container .bg4-image-2");
+	var bg4Image3 = $(".bg4-image-container .bg4-image-3");
+
+	bg4Image1.stop(true,true);
+	bg4Image2.stop(true,true);
+	bg4Image3.stop(true,true);
+
+	bg4Image1.addClass("bg4-image-ani-1");
+	bg4Image2.addClass("bg4-image-ani-2");
+	bg4Image3.addClass("bg4-image-ani-3");
 }
 
 /* 
